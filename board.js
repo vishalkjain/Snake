bdf3b19f215d9f9308d66b6372fb074e1db6430a
Vishal Jain <vkjrul3z@gmail.com>
2014-02-06 18:13:11 -0500

    Adds apples

--- board.js
+++ board.js
@@ -6,25 +6,38 @@
     this.apples = [];
   };
 
-  //gotta do
   Board.prototype.render = function(){
     var that = this;
     var string = ""
-    console.log("in render");
     for(var i = 0; i < 50; i++){
       for(var j = 0; j < 50; j++){
         if(that.snake.pos[0] == i && that.snake.pos[1] == j){
-          console.log("snake");
           string += "S     ";
+        }else if(that.apples.length > 0 && that.apples[0][0] == i && that.apples[0][1] == j){
+
+          string += "A     ";
         }else{
           string += ".     ";
         }
       }
       string += "<br>";
     }
-    //string +="";
 
     return string;
+  }
+
+  Board.prototype.addApples = function(){
+    console.log("in apples");
+    var that = this;
+    setInterval(function(){
+      if(that.apples.length == 0){
+        console.log("in apples interval");
+        var applex = Math.floor(Math.random() * 50);
+        var appley = Math.floor(Math.random() * 50);
+
+        that.apples.push([applex, appley]);
+      }
+    }, 3000);
   }
 
 })(this);
--- snake_ui.js
+++ snake_ui.js
@@ -9,20 +9,17 @@
   View.prototype.start = function(){
     this.board = new Snakes.Board();
     this.step();
+    this.board.addApples();
     console.log("startmethod");
     var that = this;
     $(document).on("keydown", function(e){
       if(e.keyCode == 37){
-        console.log("left");
         that.board.snake.turn("W");
       }else if(e.keyCode == 38){
-        console.log("up");
         that.board.snake.turn("N");
       }else if(e.keyCode == 39){
-        console.log("right");
         that.board.snake.turn("E");
       }else if(e.keyCode == 40){
-        console.log("down");
         that.board.snake.turn("S");
       }
     });
@@ -31,7 +28,6 @@
   View.prototype.step = function(){
     var that = this;
     setInterval(function(){
-      console.log("interval");
       that.board.snake.move();
       that.$el.html(that.board.render());
     }, 500);
